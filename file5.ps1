f00ff53917c315f2019278ac642c78d8b84c1d33
Iamneo <techadmin@iamneo.ai>
2023-09-14 03:55:36 +0000

    Backup Commit

--- file5.ps1
+++ file5.ps1
@@ -1,1 +1,9 @@
-New-Item -itemtype Directory -Path home/coder/project/workspace\newfolder2
+New-Item -itemtype Directory -Path home/coder/project/workspace\Testing
+New-Item -itemtype Directory -Path home/coder/project/workspace\Testing\SubFolder1
+New-Item -itemtype Directory -Path home/coder/project/workspace\Testing\SubFolder2
+1..3 | ForEach-Object {
+    New-item -ItemType File -Path "home/coder/project/workspace\Testing\SubFolder1\Test$.txt"
+}
+4...6 | ForEach-Object {
+    New-item -ItemType File -Path "home/coder/project/workspace\Testing\SubFolder2\Test$.txt"
+}
